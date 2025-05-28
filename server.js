967385b406bab5547babeabc05da09d3fd3e7a64
Vinicius de Castro Cadena <193517501+ViniCadena1405@users.noreply.github.com>
2025-05-28 13:59:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const assinaturas = require("./routes/assinantes");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -23,3 +23,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+const userRoutes = require('./routes/index');
+app.use('/', userRoutes);
